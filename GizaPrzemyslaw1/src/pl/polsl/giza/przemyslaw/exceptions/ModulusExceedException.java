69cdc813297fa5dd7e000b6d24026a0420c415bb
Myslawek <przemek.g96@wp.pl>
2019-03-31 12:03:01 +0200

    New exceptions added, together with handling

--- GizaPrzemyslaw1/src/pl/polsl/giza/przemyslaw/exceptions/ModulusExceedException.java
+++ GizaPrzemyslaw1/src/pl/polsl/giza/przemyslaw/exceptions/ModulusExceedException.java
@@ -6,14 +6,20 @@
 package pl.polsl.giza.przemyslaw.exceptions;
 
 /**
- *
- * @author Przemysław
+ * Exception which is thrown, when the value to be transformed by the RSA algorithm exceeds the set value of the modulus.
+ * @author Przemysław Giza
  */
 public class ModulusExceedException extends Exception{
-
+    /**
+     * Constructor which sets the default message for the given exception and delegates it to the class from which it inherited
+     */
     public ModulusExceedException() {
         super("The value to be encrypted/decrypted exceeds the modulus value, thus the result will not be valid.");
     }
+    /**
+     * Sets the message of the exception, as it is specified by the parameter and delegates it.
+     * @param message the message to be set as the exception message
+     */
     public ModulusExceedException(String message){
         super(message);
     }
